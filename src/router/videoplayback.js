25e61ba274b73c482ba60bf5f070884aef906993
minhpg <minhpg@gmail.com>
2021-03-13 17:52:49 +0700

    bugfix

--- src/router/videoplayback.js
+++ src/router/videoplayback.js
@@ -6,61 +6,66 @@ const request = require("request");
 const redis_client = require("../Redis");
 
 module.exports = (req, res) => {
-  if (!req.query.hash) throw new Error();
-  const referer = req.headers.referer
-  if (!referer.includes(process.env.REFERER)){
-    res.status(403).send('??????')
-  }
-  const upstream = JSON.parse(base64.decode(req.query.hash));
-  delete req.query.hash;
-  console.log(upstream);
-  const originVideo = {
-    url: upstream.domain,
-    cookie: upstream.cookie,
-  };
-  originVideo.cookie.push(process.env.COOKIE);
-  const headers = Object.assign({}, req.headers, {
-    cookie: originVideo.cookie,
-  });
-  delete headers.host;
-  delete headers.referer;
-  const ip =
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  redis_client.select(3, (err, _) => {
-    if (err) {
-      handleError(err);
+  try{  if (!req.query.hash) throw new Error();
+    const referer = req.headers.referrer
+    if (!referer.includes(process.env.REFERRER)){
+      res.status(403).send('??????')
     }
-    const time = new Date();
-    redis_client.set(ip.toString(), time);
-    console.log("NEW REQUEST FROM " + ip);
-  });
-  const playback = request({
-    url: originVideo.url,
-    method: "GET",
-    resolveWithFullResponse: true,
-    headers: headers,
-  });
-
-  playback
-    .on("response", (response) => {
-      res.statusCode = response.statusCode;
-      Object.keys(response.headers).forEach((key) => {
-        res.setHeader(key, response.headers[key]);
-      });
-    })
-    .on("error", handleError)
-    .pipe(res);
-  res.on("close", () => {
+    const upstream = JSON.parse(base64.decode(req.query.hash));
+    delete req.query.hash;
+    console.log(upstream);
+    const originVideo = {
+      url: upstream.domain,
+      cookie: upstream.cookie,
+    };
+    originVideo.cookie.push(process.env.COOKIE);
+    const headers = Object.assign({}, req.headers, {
+      cookie: originVideo.cookie,
+    });
+    delete headers.host;
+    delete headers.referer;
+    const ip =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      (req.connection.socket ? req.connection.socket.remoteAddress : null);
     redis_client.select(3, (err, _) => {
       if (err) {
         handleError(err);
       }
-      redis_client.del(ip.toString());
-      console.log("REQUEST CLOSED " + ip);
-      playback.destroy();
+      const time = new Date();
+      redis_client.set(ip.toString(), time);
+      console.log("NEW REQUEST FROM " + ip);
     });
-  });
+    const playback = request({
+      url: originVideo.url,
+      method: "GET",
+      resolveWithFullResponse: true,
+      headers: headers,
+    });
+  
+    playback
+      .on("response", (response) => {
+        res.statusCode = response.statusCode;
+        Object.keys(response.headers).forEach((key) => {
+          res.setHeader(key, response.headers[key]);
+        });
+      })
+      .on("error", handleError)
+      .pipe(res);
+    res.on("close", () => {
+      redis_client.select(3, (err, _) => {
+        if (err) {
+          handleError(err);
+        }
+        redis_client.del(ip.toString());
+        console.log("REQUEST CLOSED " + ip);
+        playback.destroy();
+      });
+    });
+  }
+  catch(err){
+    console.log(err)
+    res.send(JSON.stringify(err))
+  }
 };
