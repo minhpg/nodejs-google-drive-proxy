66183c0be1499fc025ae0e97f4b9ad514f9de370
minhpg <minhpg@gmail.com>
2021-03-16 12:00:55 +0700

    disabled referer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-'use strict'
+"use strict";
 //var heapdump = require('heapdump')
-require('dotenv').config({silent: true})
-const PORT = process.env.PORT || 6969
-const log = require('./Log')
-const server = require('./Server')
-const redisClient = require('./Redis')
+require("dotenv").config({ silent: true });
+const PORT = process.env.PORT || 6969;
+const log = require("./Log");
+const server = require("./Server");
+const redisClient = require("./Redis");
 server.listen(PORT, () => {
-  log.info(`http://0.0.0.0:${PORT}`)
-})
+  log.info(`http://0.0.0.0:${PORT}`);
+});
 
-
-setInterval(() => {
-  redisClient.select(3, () => {
-    redisClient.flushdb(() => {
-      if(err) {
-        console.log(err.message)
-      }
-      else {
-        console.log('success')
-      }
-    })
-  })
-}, 6000 * 1)
+// setInterval(() => {
+//   redisClient.select(3, (err) => {
+//     if (err) {
+//       console.log(err.message);
+//     }
+//     redisClient.flushdb((err) => {
+//       if (err) {
+//         console.log(err.message);
+//       } else {
+//         console.log("success");
+//       }
+//     });
+//   });
+// }, 6000 * 1);
