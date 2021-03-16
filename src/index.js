b6b5ed386191fbae7575fc1061ef7718f8aa0cce
minhpg <minhpg@gmail.com>
2021-03-16 11:57:06 +0700

    set interval clear stat cache

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ require('dotenv').config({silent: true})
 const PORT = process.env.PORT || 6969
 const log = require('./Log')
 const server = require('./Server')
+const redisClient = require('./Redis')
 server.listen(PORT, () => {
   log.info(`http://0.0.0.0:${PORT}`)
 })
+
+
+setInterval(() => {
+  redisClient.select(3, () => {
+    redisClient.flushdb(() => {
+      if(err) {
+        console.log(err.message)
+      }
+      else {
+        console.log('success')
+      }
+    })
+  })
+}, 6000 * 1)
